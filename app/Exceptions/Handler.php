dfc6c82781e215357f8df553d7b0916b33304f87
Maxim Paladi <paladium98@gmail.com>
2023-09-16 23:52:08 +0300

    Init project for API

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Throwable;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * The list of the inputs that are never flashed to the session on validation exceptions.
+     *
+     * @var array<int, string>
+     */
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Register the exception handling callbacks for the application.
+     */
+    public function register(): void
+    {
+        $this->reportable(function (Throwable $e) {
+            //
+        });
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param Request $request
+     * @param Throwable $e
+     * @return JsonResponse
+     */
+    public function render($request, Throwable $e): JsonResponse
+    {
+        if ($e->getMessage() == '') {
+            return response()->json(['status' => false, 'message' => "Route doesn't exist."], 404);
+        }
+
+        return response()->json(['status' => false, 'message' => $e->getMessage()]);
+    }
+}
